ad8bfd649ef4214f3ef284622222b699615ea61e
Ouderbijslag <KerimOfficieel@gmail.com>
2022-02-18 10:40:33 +0100

    3.5

--- 3-Arrays/3.5 - Array sorteren/index.php
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -3,7 +3,13 @@
 <body>
 
 <?php
+$laag_hoog = array("21","46","47","676","5345","78657");
+asort($laag_hoog);
+print_r($laag_hoog);
 
+$hoog_laag = array("78657","5345","676","47","46","21");
+arsort($hoog_laag);
+print_r($hoog_laag);
 ?>
 
 </body>
