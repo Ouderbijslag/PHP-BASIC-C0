4178cee9cc0a7433beadab8e5dce40ea4805de15
Ouderbijslag <KerimOfficieel@gmail.com>
2022-02-18 10:03:07 +0100

    commit 3.3

--- 3-Arrays/3.3 - Array waarde toevoegen/index.php
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -3,7 +3,16 @@
 <body>
 
 <?php
+$myheroes = array ('<li>Spiderman</li>' , '<li>Wolverine</li>' , '<li>Black Panther</li>');
+$myheroes[3] = '<li>Valkyrie</li>';
 
+array_push ($myheroes, '<li>Deadpool</li>');
+
+echo $myheroes[0];
+echo $myheroes[1];
+echo $myheroes[2];
+echo $myheroes[3];
+echo $myheroes[4];
 ?>
 
 </body>
