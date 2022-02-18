576b8b5aa9749ce1d04bfb31cabc7f63ae8ab1b0
Ouderbijslag <KerimOfficieel@gmail.com>
2022-02-18 09:51:18 +0100

    commit 3.2

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -3,7 +3,17 @@
 <body>
 
 <?php
+$MyDogNames = ['<li>Mick</li> ', '<li>Vincent</li> ', '<li>Bob</li> '];
+echo $MyDogNames[0];
+echo $MyDogNames[1];
+echo $MyDogNames[2];
 
+$mydognames[1] = '<li>Lof</li>';
+$mydognames[0] = '<li>Rayan</li>';
+$mydognames[2] = '<li>Sem</li>';
+echo $mydognames[1];
+echo $mydognames[0];
+echo $mydognames[2];
 ?>
 
 </body>
