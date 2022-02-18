b5c1d137f87c0c98e32110f19afdf6755407ead3
Ouderbijslag <KerimOfficieel@gmail.com>
2022-02-18 10:30:42 +0100

    commit 3.4

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -3,7 +3,11 @@
 <body>
 
 <?php
-
+    $abcGetallen = [ ["A", "B", " C", "D"], [24,56,78,90] ];
+    echo $abcGetallen[0][0];
+    echo $abcGetallen[1][1];
+    echo $abcGetallen[0][2];
+    echo $abcGetallen[1][3];
 ?>
 
 </body>
