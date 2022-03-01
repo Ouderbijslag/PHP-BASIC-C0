9531fb359a06d692c2c50cbb69425142bad2f014
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-01 13:54:39 +0100

    4.2 commit

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -3,7 +3,22 @@
 <body>
 
 <?php
+$getal1 = 10;
+$getal1 += 5;
+echo $getal1 . "<br>";
 
+$getal1 = 10;
+$getal1 -= 5;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 *= 5;
+echo $getal1 . "<br>";
+
+
+$getal1 = 10;
+$getal1 /= 5;
+echo $getal1 . "<br>";
 ?>
 
 </body>
