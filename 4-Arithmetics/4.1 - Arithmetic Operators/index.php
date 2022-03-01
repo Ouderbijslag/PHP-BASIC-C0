ac3b05388b70d5f01a701d1d1627f52f245d83cf
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-01 13:48:17 +0100

    4.2 commit

--- 4-Arithmetics/4.1 - Arithmetic Operators/index.php
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -3,12 +3,22 @@
 <body>
 
 <?php
-$getal1 = 5;
-$getal2 = 10;
-echo $getal1 + $getal2;
-echo $getal1 - $getal2;
-echo $getal1 / $getal2;
-echo $getal1 * $getal2;
+$getal1 = 10;
+$getal1 += 5;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 -= 5;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 *= 5;
+echo $getal1 . "<br>";
+
+
+$getal1 = 10;
+$getal1 /= 5;
+echo $getal1 . "<br>";
 ?>
 
 </body>
