787b1e874634fc53fed7a2536f95f0442c60f912
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-02 12:08:27 +0100

    5.1 commit

--- 5-Conditional_Statements/5.1 - Comparison Operators/index.php
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -3,7 +3,16 @@
 <body>
 
 <?php
-
+$_tekst_1 = 12;
+$_tekst_2 = "gewoon tekst";
+var_dump( $_tekst_1 == $_tekst_1);
+var_dump($_tekst_1 === $_tekst_2);
+var_dump($_tekst_1 != $_tekst_2);
+var_dump($_tekst_1 > $_tekst_2);
+var_dump($_tekst_1 < $_tekst_2);
+var_dump($_tekst_1 <> $_tekst_2);
+var_dump($_tekst_1 >= $_tekst_2);
+var_dump($_tekst_1 <= $_tekst_2);
 ?>
 
 </body>
