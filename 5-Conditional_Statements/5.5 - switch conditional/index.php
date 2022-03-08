4eab366fe4757a9fd12b3c07b543424742bd58e4
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-08 17:36:41 +0100

    5.5 commit

--- 5-Conditional_Statements/5.5 - switch conditional/index.php
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -3,7 +3,24 @@
 <body>
 
 <?php
+$score = 20;
 
+switch($score) {
+    case 20:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 19:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 15:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 10:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
 ?>
 
 </body>
