bb339f5c4a2185974ef151426f41cac082eb5024
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-11 10:32:15 +0100

    5.6 commit

--- 5-Conditional_Statements/5.6 - Logical Operators/index.php
+++ 5-Conditional_Statements/5.6 - Logical Operators/index.php
@@ -3,7 +3,32 @@
 <body>
 
 <?php
+$var1 = 3;
+$var2 = 6;
 
+if($var1 < 20 && $var2 > 5) {
+    echo "Het getal zit tussen de 20 en de 5";
+} else{
+    echo "het getal is groter dan 20 of kleiner dan 5!";
+}
+echo "<br>";
+if($var1 = 10 || $var2 > 5){
+    echo "Of het een";
+} else{
+    echo "Of het ander";
+}
+echo "<br>";
+if($var1 == 3 xor $var2 > 5){
+    echo "XOR 1";
+} else{
+    echo "XOR 2";
+}
+echo "<br>";
+if($var1 != 3 && $var2 > 5){
+    echo "True als de vergelijking niet waar is";
+} else{
+    echo "False";
+}
 ?>
 
 </body>
