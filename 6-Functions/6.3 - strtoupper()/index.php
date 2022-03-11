65449eb8d590be0b2ef92d039e8450140b36c01c
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-11 12:09:56 +0100

    6.3 commit

--- 6-Functions/6.3 - strtoupper()/index.php
+++ 6-Functions/6.3 - strtoupper()/index.php
@@ -3,7 +3,13 @@
 <body>
 
 <?php
+function telAlleWoorden(){
+    $var1 = "Mijn blauwe auto is een mooie snelle wagen";
+    $lengteZin = strlen($var1);
+    echo $lengteZin;
+}
 
+telAlleWoorden();
 ?>
 
 </body>
