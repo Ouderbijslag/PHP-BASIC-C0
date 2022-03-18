46f56e84b4716c96d1b600f75c74d0c01982870b
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-18 10:04:11 +0100

    6.4 commit

--- 6-Functions/6.4 - return statement/index.php
+++ 6-Functions/6.4 - return statement/index.php
@@ -3,7 +3,20 @@
 <body>
 
 <?php
+    function geefTienProcent(){
+        $koopprijs = 75;
+        $korting = 10;
+        $totaal = 100;
+        return ($koopprijs/$totaal)*$korting;
+    }
 
+    function zetAllesgroot(){
+        $koopprijs = 75;
+        $totaalkorting =$koopprijs - geefTienProcent();
+        echo "Het totaalbedrag is $totaalkorting";
+    }
+
+    zetAllesgroot();
 ?>
 
 </body>
