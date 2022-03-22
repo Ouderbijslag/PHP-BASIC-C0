b4f0667bca48cc42983db76399a85cc861b5728c
Ouderbijslag <KerimOfficieel@gmail.com>
2022-03-22 13:00:51 +0100

    7.1 commit verbeterd

--- 7-Working with files/7.1 - include/kerim.php
+++ 7-Working with files/7.1 - include/kerim.php
@@ -7,13 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <div class="menu">
-     
-    
-    <?php include 'includes/kerim.php';?>
-    
-    </div>
-   
+    <ul>
+        <li>menu-item 1</li>
+        <li>menu-item 2</li>
+        <li>menu-item 3</li>
+    </ul>
 </body>
 </html>
